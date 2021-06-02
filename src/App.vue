3e4ef97ebbf940f3d2cfe2af41d5a8ccd3e993db
Thomas Amland <thomas.amland@gmail.com>
2021-06-02 17:33:16 +0200

    add modal example
    does not work

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,9 @@
     <b-btn variant="primary" @click="count += 1">
       Click me
     </b-btn>
+    <b-modal :visible="true" title="BootstrapVue">
+      <p class="my-4">Hello from modal!</p>
+    </b-modal>
   </div>
 </template>
 <script>
