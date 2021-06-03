fc86b14c86ff6e6bdd22621444f4b619f0fcd6d8
Thomas Amland <thomas.amland@gmail.com>
2021-06-03 18:01:57 +0200

    dropdown

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,16 @@
     <b-modal :visible="true" title="BootstrapVue">
       <p class="my-4">Hello from modal!</p>
     </b-modal>
+    <div>
+      <b-dropdown id="dropdown-1" text="Dropdown Button" class="m-md-2">
+        <b-dropdown-item>First Action</b-dropdown-item>
+        <b-dropdown-item>Second Action</b-dropdown-item>
+        <b-dropdown-item>Third Action</b-dropdown-item>
+        <b-dropdown-divider></b-dropdown-divider>
+        <b-dropdown-item active>Active action</b-dropdown-item>
+        <b-dropdown-item disabled>Disabled action</b-dropdown-item>
+      </b-dropdown>
+    </div>
   </div>
 </template>
 <script>
